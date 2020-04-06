8a07498937fcc1f768fdb2708ff9b4a7cf152bc8
MomoKrono <momos1796@gmail.com>
2020-04-06 16:32:43 +0200

    added lables prototypes on axes

--- src/plotter.cpp
+++ src/plotter.cpp
@@ -37,7 +37,6 @@ namespace spl
             buffer.at(base_offset + 2) = 0x00;
         }
 
-
         // PMM FILE FORMAT
         // 1) 'P3'
         // 2) width height
@@ -53,13 +52,19 @@ namespace spl
 
     void plotter::show() const
     {
+        namespace rvw = ranges::views;
+        // anti aliasing, ma non mi piace l'effetto
+        // sf::ContextSettings ctx;
+        // ctx.antialiasingLevel=2;
+        // sf::RenderWindow window(sf::VideoMode(_width, _height), "titolo", sf::Style::Default, ctx);
 
         sf::RenderWindow window(sf::VideoMode(_width, _height), "titolo");
         sf::VertexArray axes;
 
         sf::VertexArray vertexes{sf::LinesStrip};   // TODO: aggiungere la possibilità di cambiare primitiva
-        axes.setPrimitiveType(sf::Lines);           // teper poter fare linee tratteggiate, cerchi, tratto punto, ecc
-        
+        axes.setPrimitiveType(sf::Lines);           // per poter fare linee tratteggiate, cerchi, tratto punto, ...
+                                                    // Lines, LinesStrip, Points come primitive di un vertex array,
+                                                    // altrimenti si va di vettore di cerchi, quadrati, stelline, ...
         auto points = get_plot_points();
         for (auto [x, y] : points)
         {
@@ -70,6 +75,13 @@ namespace spl
         }
 
         // TODO: abbellire sta roba, aggiungere le misure sugli assi
+        std::vector<sf::Text> labels;
+        sf::Font font;
+        font.loadFromFile("arial.ttf"); // magari usare un font di sistema
+        sf::Text label;
+        label.setFont(font);
+        label.setFillColor(sf::Color::Black);
+        label.setCharacterSize(10);
 
         auto const [x_min, x_max] = std::minmax_element(_xs.begin(), _xs.end());
         auto const [y_min, y_may] = std::minmax_element(_ys.begin(), _ys.end());
@@ -77,14 +89,39 @@ namespace spl
         auto const y = plotter::rescale(*y_min, *y_may, _height, 0.);
         sf::Vertex vert;
         vert.color = sf::Color::Black;
+        // asse x
         vert.position = sf::Vector2f(x,_border);
         axes.append(vert);
         vert.position = sf::Vector2f(x,_height-_border);
         axes.append(vert);
+        // asse y
         vert.position = sf::Vector2f(_border,y);
         axes.append(vert);
         vert.position = sf::Vector2f(_width-_border,y);
         axes.append(vert);
+        // qui ci sono i segnetti sugli assi, 10 per asse
+        // TODO: numero modificabile? diverso per x e y? aggiungere le misure
+        for (auto i{_border}; i <= _width-_border; i+=(_width-2*_border)/10)
+        {
+            vert.position = sf::Vector2f(i, y+4);
+            axes.append(vert);
+            vert.position = sf::Vector2f(i, y-4);
+            axes.append(vert);
+            label.setString(fmt::format("{}", i)); // TODO: inserire qui il numero su R della mia funzione, non i pixel, nel vettore _xs
+            label.setPosition(sf::Vector2f(i, y)); // questo è ok
+            labels.push_back(label);
+        }
+        for (auto j{_border}; j <= _height-_border; j+=(_height-2*_border)/10)
+        {
+            vert.position = sf::Vector2f(x+4, j);
+            axes.append(vert);
+            vert.position = sf::Vector2f(x-5, j);
+            axes.append(vert);
+            label.setString(fmt::format("{}", j)); // TODO: come sopra, vettore _ys
+            label.setPosition(sf::Vector2f(x, j)); // questo è ok
+            labels.push_back(label);
+        }
+
 
         while (window.isOpen())
         {
@@ -106,8 +143,26 @@ namespace spl
                         sf::RenderTexture texture;
                         texture.create(_width, _height);
                         texture.clear(sf::Color::White);
-                        texture.draw(axes);
-                        texture.draw(vertexes);
+                        auto reflect = [&](sf::Vertex v){
+                                v.position.y = -v.position.y;
+                        };
+                        auto ax = axes;
+                        auto vx = vertexes;
+                        for (std::size_t i{0}; i < vx.getVertexCount(); ++i)
+                        {
+                            reflect(vx[i]);
+                        }
+                        for (std::size_t i{0}; i < ax.getVertexCount(); ++i)
+                        {
+                            reflect(ax[i]);
+                        }
+                        // auto axs = axes | rvw::transform([](auto v){reflect(v);});
+                        texture.draw(ax);
+                        texture.draw(vx);
+                        for (auto & l : labels)
+                        {
+                            texture.draw(l);
+                        }
                         texture.getTexture().copyToImage().saveToFile("plot.png"); // TODO: cin >> nome, tipo di file
                         std::cout << "plot saved" << std::endl; // TODO: aggiungere un testo su schermo
                     }
@@ -117,6 +172,10 @@ namespace spl
             window.clear(sf::Color::White);
             window.draw(axes);
             window.draw(vertexes);
+            for (auto & l : labels)
+            {
+                window.draw(l);
+            }
             window.display();
         }
     }
@@ -131,6 +190,5 @@ namespace spl
         auto const ys = _ys | rvw::transform(std::bind_front(&plotter::rescale, this, *y_min, *y_may, _height));
 
         return rvw::zip(xs, ys) | ranges::to<std::vector<std::pair<int, int>>>;
-
     }
 } // namespace spl
