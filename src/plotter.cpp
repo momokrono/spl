12ec9daa8f0cead2aa4fe4296cc9f4a0f4d35a93
rbrugo <brugo.riccardo@gmail.com>
2020-04-24 19:36:38 +0200

    Moved axes and labels generation to a separate function

--- src/plotter.cpp
+++ src/plotter.cpp
@@ -13,37 +13,13 @@
 
 namespace spl
 {
-    void plotter::show() const
+    auto plotter::generate_labels(int const number_of_labels_x, int const number_of_labels_y) const
+        -> std::pair<sf::VertexArray, std::vector<sf::Text>>
     {
-        constexpr auto number_of_labels_x = 10;
-        constexpr auto number_of_labels_y = 10;
-        sf::RenderWindow window(sf::VideoMode(_width, _height), "titolo");
+        // configure axis
         sf::VertexArray axes;
+        axes.setPrimitiveType(sf::Lines);
 
-        sf::VertexArray function{sf::LinesStrip};   // TODO: aggiungere la possibilità di cambiare primitiva
-        axes.setPrimitiveType(sf::Lines);           // per poter fare linee tratteggiate, cerchi, tratto punto, ...
-                                                    // Lines, LinesStrip, Points come primitive di un vertex array,
-                                                    // altrimenti si va di vettore di cerchi, quadrati, stelline, ...
-        auto points = get_plot_points();
-        for (auto const [x, y] : points)
-        {
-            auto const point = sf::Vertex{{x + 0.f, _height - y + 0.f}, sf::Color::Blue};
-            function.append(point);
-            // function.append({{x + 0.f, _height - y + 0.f}, sf::Color::Blue}); // or this
-        }
-
-        // TODO: abbellire sta roba, aggiungere le misure sugli assi
-        std::vector<sf::Text> labels; labels.reserve(number_of_labels_x + number_of_labels_y);
-
-        sf::Font font;
-        font.loadFromFile("arial.ttf"); // magari usare un font di sistema
-
-        sf::Text label;
-        label.setFont(font);
-        label.setFillColor(sf::Color::Black);
-        label.setCharacterSize(10);
-
-        // draw axis
         auto const [x_min, x_max] = std::minmax_element(_xs.begin(), _xs.end());
         auto const [y_min, y_max] = std::minmax_element(_ys.begin(), _ys.end());
         auto const x = plotter::rescale(*x_min, *x_max, _width, 0.);
@@ -55,23 +31,26 @@ namespace spl
         axes.append({sf::Vector2f{         _border + 0.f, y + 0.f}, sf::Color::Black});
         axes.append({sf::Vector2f{_width - _border + 0.f, y + 0.f}, sf::Color::Black});
 
-        // qui ci sono i segnetti sugli assi
-        // TODO: Aggiungere le misure
-        sf::Vertex vert;
-        vert.color = sf::Color::Black;
+        // configure labels
+        std::vector<sf::Text> labels; labels.reserve(number_of_labels_x + number_of_labels_y);
+        sf::Font font;
+        font.loadFromFile("arial.ttf"); // magari usare un font di sistema
+
+        sf::Text label;
+        label.setFont(font);
+        label.setFillColor(sf::Color::Black);
+        label.setCharacterSize(10);
+
         auto const width_step = (_width - 2. * _border) / number_of_labels_x;
         auto const x_step     = (*x_max - *x_min)       / number_of_labels_x;
         for (auto n{0}; n <= number_of_labels_x; ++n)
         {
             auto const x_px = _border + width_step * n;
             auto const x_fn = *x_min + x_step * n;
-            vert.position = sf::Vector2f(x_px, y + 4);
-            axes.append(vert);
-            vert.position = sf::Vector2f(x_px, y - 4);
-            axes.append(vert);
-            // if (n == 5)
-            //     continue; // bruttissimo
+            axes.append({sf::Vector2f(x_px, y + 4), sf::Color::Black});
+            axes.append({sf::Vector2f(x_px, y - 4), sf::Color::Black});
             label.setString(fmt::format("{:.2}", x_fn));
+            fmt::print(stderr, "{}\n", label.getString().toAnsiString());
             label.setPosition(x_px, y);
             labels.push_back(label);
         }
@@ -82,10 +61,8 @@ namespace spl
         {
             auto const y_px = _border + height_step * m;
             auto const y_fn = - (*y_min + y_step * m);
-            vert.position = sf::Vector2f(x + 4, y_px);
-            axes.append(vert);
-            vert.position = sf::Vector2f(x - 4, y_px);
-            axes.append(vert);
+            axes.append({sf::Vector2f(x + 4, y_px), sf::Color::Black});
+            axes.append({sf::Vector2f(x - 4, y_px), sf::Color::Black});
             if (m != number_of_labels_y / 2) {
                 label.setString(fmt::format("{:.2}", y_fn));
                 label.setPosition(x, y_px);
@@ -93,7 +70,101 @@ namespace spl
             }
         }
 
+        return {axes, labels};
+    }
 
+    void plotter::show() const
+    {
+        constexpr auto number_of_labels_x = 10;
+        constexpr auto number_of_labels_y = 10;
+        sf::RenderWindow window(sf::VideoMode(_width, _height), "titolo");
+        /* sf::VertexArray axes; */
+
+        sf::VertexArray function{sf::LinesStrip};   // TODO: aggiungere la possibilità di cambiare primitiva
+        /* axes.setPrimitiveType(sf::Lines);           // per poter fare linee tratteggiate, cerchi, tratto punto, ... */
+                                                    // Lines, LinesStrip, Points come primitive di un vertex array,
+                                                    // altrimenti si va di vettore di cerchi, quadrati, stelline, ...
+        auto points = get_plot_points();
+        for (auto const [x, y] : points)
+        {
+            auto const point = sf::Vertex{{x + 0.f, _height - y + 0.f}, sf::Color::Blue};
+            function.append(point);
+            // function.append({{x + 0.f, _height - y + 0.f}, sf::Color::Blue}); // or this
+        }
+
+        // TODO: abbellire sta roba, aggiungere le misure sugli assi
+
+        /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        /* std::vector<sf::Text> labels; labels.reserve(number_of_labels_x + number_of_labels_y); */
+
+        /* sf::Font font; */
+        /* font.loadFromFile("arial.ttf"); // magari usare un font di sistema */
+
+        /* sf::Text label; */
+        /* label.setFont(font); */
+        /* label.setFillColor(sf::Color::Black); */
+        /* label.setCharacterSize(10); */
+        /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        /* // draw axis */
+        /* auto const [x_min, x_max] = std::minmax_element(_xs.begin(), _xs.end()); */
+        /* auto const [y_min, y_max] = std::minmax_element(_ys.begin(), _ys.end()); */
+        /* auto const x = plotter::rescale(*x_min, *x_max, _width, 0.); */
+        /* auto const y = plotter::rescale(*y_min, *y_max, _height, 0.); */
+        /* // asse x */
+        /* axes.append({sf::Vector2f{x + 0.f,           _border + 0.f}, sf::Color::Black}); */
+        /* axes.append({sf::Vector2f{x + 0.f, _height - _border + 0.f}, sf::Color::Black}); */
+        /* // asse y */
+        /* axes.append({sf::Vector2f{         _border + 0.f, y + 0.f}, sf::Color::Black}); */
+        /* axes.append({sf::Vector2f{_width - _border + 0.f, y + 0.f}, sf::Color::Black}); */
+
+        // qui ci sono i segnetti sugli assi
+        // TODO: Aggiungere le misure
+        /* sf::Vertex vert; */
+        /* vert.color = sf::Color::Black; */
+        /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ separare vertici e lables
+        /* auto const width_step = (_width - 2. * _border) / number_of_labels_x; */
+        /* auto const x_step     = (*x_max - *x_min)       / number_of_labels_x; */
+        /* for (auto n{0}; n <= number_of_labels_x; ++n) */
+        /* { */
+        /*     auto const x_px = _border + width_step * n; */
+        /*     auto const x_fn = *x_min + x_step * n; */
+        /*     axes.append({sf::Vector2f(x_px, y + 4), sf::Color::Black}); */
+        /*     /1* vert.position = sf::Vector2f(x_px, y + 4); *1/ */
+        /*     /1* axes.append(vert); *1/ */
+        /*     axes.append({sf::Vector2f(x_px, y - 4), sf::Color::Black}); */
+        /*     /1* vert.position = sf::Vector2f(x_px, y - 4); *1/ */
+        /*     /1* axes.append(vert); *1/ */
+        /*     // if (n == 5) */
+        /*     //     continue; // bruttissimo */
+        /*     label.setString(fmt::format("{:.2}", x_fn)); */
+        /*     label.setPosition(x_px, y); */
+        /*     labels.push_back(label); */
+        /* } */
+
+        /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ separare vertici e lables
+        /* auto const height_step = (_height - 2 * _border) / number_of_labels_y; */
+        /* auto const y_step      = (*y_max - *y_min)       / number_of_labels_y; */
+        /* for (auto m{0}; m <= number_of_labels_y; ++m) */
+        /* { */
+        /*     auto const y_px = _border + height_step * m; */
+        /*     auto const y_fn = - (*y_min + y_step * m); */
+        /*     vert.position = sf::Vector2f(x + 4, y_px); */
+        /*     axes.append(vert); */
+        /*     vert.position = sf::Vector2f(x - 4, y_px); */
+        /*     axes.append(vert); */
+        /*     if (m != number_of_labels_y / 2) { */
+        /*         label.setString(fmt::format("{:.2}", y_fn)); */
+        /*         label.setPosition(x, y_px); */
+        /*         labels.push_back(label); */
+        /*     } */
+        /* } */
+
+
+        auto const [axes, labels] = generate_labels(number_of_labels_x, number_of_labels_y);
+        for (auto const & label : labels) {
+            fmt::print(stderr, "> {}\n", label.getString().toAnsiString());
+        }
         while (window.isOpen())
         {
             sf::Event event;
