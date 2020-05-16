f38d387ed8a26d8704d6999d16fab73eedd1b621
MomoKrono <momos1796@gmail.com>
2020-05-16 21:37:14 +0200

    updated Image.hpp

--- include/Graphics/Image.hpp
+++ include/Graphics/Image.hpp
@@ -2,13 +2,15 @@
 #define _IMAGE_HPP_
 
 #include<vector>
-// #include <cstdint>
-#include <iostream>
+#include <cstdint>
+// #include <iostream>
+
+#include "bits/Iterators.hpp"
 
 namespace spl::graphics
 {
 
-union rgba
+struct rgba
 {
     uint8_t r;
     uint8_t g;
@@ -16,17 +18,17 @@ union rgba
     uint8_t a;
 };
 
-class Image
+class image
 {
 public:
     Image() : _width{0}, _height{0} {};
     Image( int w, int h ) : _width{w}, _height{h} {};
     auto get_pixel( int const x, int const y)  -> rgba;
-    auto access_row( int const y ) -> std::vector<std::vector<rgba>>::iterator;
+    auto access_row( int const y ) -> std::vector<rgba>::iterator;
     auto access_column( int const x ) -> std::vector<rgba>::iterator;
     auto access_pixel( int const x, int const y ) -> std::pair<std::vector<std::vector<rgba>>::iterator, std::vector<rgba>::iterator>;
 private:
-    std::vector<std::vector<rgba>> _pixels;
+    std::vector<rgba> _pixels;
     int _width, _height;
 };
 
