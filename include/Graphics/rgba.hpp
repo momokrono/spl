da792bcb0f1335b13ed748c0f1231015a5f69e28
rbrugo <brugo.riccardo@gmail.com>
2020-06-04 01:06:01 +0200

    New constructors

--- include/Graphics/rgba.hpp
+++ include/Graphics/rgba.hpp
@@ -7,6 +7,8 @@ namespace spl::graphics
 {
 struct rgba
 {
+    rgba() = default;
+    rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255) : r{r}, g{g}, b{b}, a{a} {}
     uint8_t r;
     uint8_t g;
     uint8_t b;
@@ -14,7 +16,6 @@ struct rgba
 
     friend bool operator==(rgba const, rgba const) = default;
 };
-
 } //namespace spl::graphics
 
 #endif
