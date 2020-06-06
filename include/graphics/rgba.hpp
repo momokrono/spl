90fc61a7cf3ae5cdb5f538c7dd716f5a0c178871
rbrugo <brugo.riccardo@gmail.com>
2020-06-07 00:35:07 +0200

    New namespace `color`

--- include/graphics/rgba.hpp
+++ include/graphics/rgba.hpp
@@ -7,15 +7,26 @@ namespace spl::graphics
 {
 struct rgba
 {
-    rgba() = default;
-    rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255) : r{r}, g{g}, b{b}, a{a} {}
+    constexpr rgba() = default;
+    constexpr rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255) : r{r}, g{g}, b{b}, a{a} {}
     uint8_t r;
     uint8_t g;
     uint8_t b;
     uint8_t a;
 
+    constexpr
     friend bool operator==(rgba const, rgba const) = default;
 };
+
+namespace color
+{
+    constexpr const rgba red     {255,   0,      0,      255};
+    constexpr const rgba green   {0,     255,    0,      255};
+    constexpr const rgba blue    {0,     0,      255,    255};
+    constexpr const rgba white   {255,   255,    255,    255};
+    constexpr const rgba black   {0,     0,      0,      255};
+    constexpr const rgba nothing {0,     0,      0,      0};
+} // namespace color
 } //namespace spl::graphics
 
 #endif
