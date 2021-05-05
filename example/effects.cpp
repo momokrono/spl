7b7e42ef62ee52fb614561b41f3eb73049580edf
Giacomo Angelucci <momos1796@gmail.com>
2021-05-05 12:19:54 +0200

    first implementation of triangular bloom inside effects

--- example/effects.cpp
+++ example/effects.cpp
@@ -6,6 +6,8 @@ int main()
 {
 	auto mario = spl::graphics::image{};
 	mario.load_from_file("mario.jpg");
-	spl::graphics::effects::greyscale(std::in_place, mario);
-	mario.save_to_file("mario_bw.jpg");
+	auto mario_bw = spl::graphics::effects::greyscale(mario);
+	mario_bw.save_to_file("mario_bw.jpg");
+	spl::graphics::effects::triangular_blur(std::in_place, mario, 5);
+	mario.save_to_file("mario_blurred.jpg");
 }
--- include/spl/effects.hpp
+++ include/spl/effects.hpp
@@ -42,6 +42,129 @@ spl::graphics::image greyscale(image && img)
 	return res;
 }
 
+void triangular_blur(std::in_place_t, spl::graphics::viewport original, int16_t radius)
+{
+	auto compute_blur = [&](int64_t x0, int64_t y0, spl::graphics::viewport const & img, int16_t radius) noexcept
+    -> spl::graphics::rgba
+{
+    radius = std::min({x0, y0, img.swidth() - x0, img.sheight() - y0, radius + 0l});
+    if (radius == 0) { radius = 1; }
+    auto triangular_filter = [=](int64_t dx, int64_t dy) -> double {
+        if (dx + dy < radius) {
+            return (1 - (dx + dy) * 1. / radius);
+        } else {
+            return 0;
+        }
+    };
+
+    auto total_contribution = 0.;
+
+    double r = 0;
+    double g = 0;
+    double b = 0;
+    auto const max_x = std::min(img.swidth(), x0 + radius);
+    auto const max_y = std::min(img.sheight(), y0 + radius);
+    for (auto y = std::max(0l, y0 - radius); y < max_y; ++y) {
+        auto partial_r = 0.;
+        auto partial_g = 0.;
+        auto partial_b = 0.;
+        for (auto x = std::max(0l, x0 - radius); x < max_x; ++x) {
+            auto weight = triangular_filter(std::abs(x - x0), std::abs(y - y0));
+            auto color  = img.pixel(x, y);
+
+            partial_r += weight * color.r * color.r;
+            partial_g += weight * color.g * color.g;
+            partial_b += weight * color.b * color.b;
+            total_contribution += weight;
+        }
+
+        r += partial_r;
+        g += partial_g;
+        b += partial_b;
+    }
+
+    auto const norm_factor = 1. / total_contribution;
+    r *= norm_factor;
+    g *= norm_factor;
+    b *= norm_factor;
+
+    return spl::graphics::rgba(std::sqrt(r), std::sqrt(g), std::sqrt(b));
+};
+	auto buffer = original;
+	for (auto x = 0; x < original.swidth(); ++x) {
+		for (auto y = 0; y < original.sheight(); ++y) {
+			original.pixel(x, y) = compute_blur(x, y, buffer, radius);
+		}
+	}
+}
+
+[[nodiscard]]
+spl::graphics::image triangular_blur(spl::graphics::viewport v, int16_t r)
+{
+	auto img = spl::graphics::image{v};
+	triangular_blur(std::in_place, img, r);
+	return img;
+}
+
+[[nodiscard]]
+spl::graphics::image triangular_blur(viewport && img, int16_t r)
+{
+	auto res = std::move(img);
+	triangular_blur(std::in_place, res, r);
+	return res;
+}
+ /* FIXME : loopa e non esce
+void box_blur(std::in_place_t, spl::graphics::viewport original, int16_t radius) {
+	auto compute_blur = [](int64_t x0, int64_t y0, spl::graphics::image const &img, int16_t radius) noexcept {
+		radius = std::min({x0, y0, img.swidth() - x0, img.sheight() - y0, radius + 0l});
+		if (radius == 0) { radius = 1; }
+		double r = 0;
+		double g = 0;
+		double b = 0;
+		auto const norm_factor = 1. / ((2 * radius + 1) * (2 * radius + 1));
+		auto const max_x = std::min(img.swidth(), x0 + radius + 1);
+		auto const max_y = std::min(img.sheight(), y0 + radius + 1);
+		for (auto y = std::max(0l, y0 - radius); y < max_y; ++y) {
+			auto partial_r = 0.;
+			auto partial_g = 0.;
+			auto partial_b = 0.;
+			for (auto x = std::max(0l, x0 - radius); x < max_x; ++x) {
+				auto color = img.pixel(x, y);
+
+				partial_r += color.r * color.r;
+				partial_g += color.g * color.g;
+				partial_b += color.b * color.b;
+			}
+			r += partial_r * norm_factor;
+			g += partial_g * norm_factor;
+			b += partial_b * norm_factor;
+		}
+		return spl::graphics::rgba(std::sqrt(r), std::sqrt(g), std::sqrt(b));
+	};
+	auto buffer = original;
+	for (auto x = 0; x < original.swidth(); ++x) {
+		for (auto y = 0; y < original.sheight(); ++y) {
+			original.pixel(x, y) = compute_blur(x, y, buffer, radius);
+		}
+	}
+}
+
+[[nodiscard]]
+spl::graphics::image box_blur(spl::graphics::viewport v, int16_t r)
+{
+	auto img = spl::graphics::image{v};
+	box_blur(std::in_place, img, r);
+	return img;
+}
+
+[[nodiscard]]
+spl::graphics::image box_blur(viewport && img, int16_t r)
+{
+	auto res = std::move(img);
+	box_blur(std::in_place, res, r);
+	return res;
+}
+*/
 } // namespace spl::graphics::effects
 
 #endif /* EFFECTS_HPP */
