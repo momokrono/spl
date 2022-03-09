c97f2063e152c372531079441710f10e62f1d214
rbrugo <brugo.riccardo@gmail.com>
2022-03-09 21:11:20 +0100

    Switch to fmt 8.1.1

--- example/bench.cpp
+++ example/bench.cpp
@@ -15,7 +15,7 @@ int main(int argc, [[maybe_unused]] char * argv[]) try
 {
     auto image = spl::graphics::image(600,400);
 
-    fmt::print(stdout, argc == 1 ? "new\n" : "old\n");
+    fmt::print(stdout, "{}", argc == 1 ? "new\n" : "old\n");
     for (int j = 0; j <= 66; ++j) {
         auto line1 = spl::graphics::line{{0,   200}, {599, 200 + j * 3 }, spl::graphics::color::green};
         auto line2 = spl::graphics::line{{599, 200}, {  0, 200 + j * 3 }, spl::graphics::color::blue};
